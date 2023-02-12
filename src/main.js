ec7462fb6cc1a5079b450ec28a3c2dd384aad1bc
mygit <purceladzedavit@gmail.com>
2023-02-12 15:36:12 +0400

    add personal page with css styles

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import App from "./App.vue";
 import TheHome from "./components/TheHome.vue";
+import pagePersonal from "./pages/pagePersonal.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/RedBerry", component: TheHome }],
+  routes: [
+    { path: "/RedBerry", component: TheHome },
+    { path: "/RedBerry/pagepersonal", component: pagePersonal },
+  ],
 });
 const app = createApp(App);
 
